77938c58589c69a9b84a470aaf73aaf6565e2867
Ilya Maltsev <ilya.maltsev@reltio.com>
2019-04-15 00:02:59 +0500

    Сайт с рейтингами

--- src/main/java/ru/appliedtech/chess/playoffsite/PlayoffSiteGenerator.java
+++ src/main/java/ru/appliedtech/chess/playoffsite/PlayoffSiteGenerator.java
@@ -8,9 +8,6 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 import ru.appliedtech.chess.*;
-import ru.appliedtech.chess.elorating.EloRating;
-import ru.appliedtech.chess.elorating.KValue;
-import ru.appliedtech.chess.elorating.KValueSet;
 import ru.appliedtech.chess.playoff.PlayoffLevel;
 import ru.appliedtech.chess.playoff.PlayoffLevelPlayers;
 import ru.appliedtech.chess.playoff.PlayoffLevelTable;
@@ -18,17 +15,20 @@ import ru.appliedtech.chess.playoff.PlayoffSetup;
 import ru.appliedtech.chess.playoff.io.PlayoffSetupObjectNodeReader;
 import ru.appliedtech.chess.playoffsite.level_table.PlayoffLevelTableView;
 import ru.appliedtech.chess.playoffsite.level_table.PlayoffLevelViewHtmlRenderingEngine;
-import ru.appliedtech.chess.storage.*;
+import ru.appliedtech.chess.storage.GameReadOnlyStorage;
+import ru.appliedtech.chess.storage.GameStorage;
+import ru.appliedtech.chess.storage.PlayerReadOnlyStorage;
+import ru.appliedtech.chess.storage.PlayerStorage;
 
 import java.io.*;
-import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Comparator.comparingInt;
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class PlayoffSiteGenerator {
 
@@ -39,17 +39,13 @@ public class PlayoffSiteGenerator {
                 args[2],
                 args[3],
                 args[4],
-                args[5],
-                args[6],
-                args[7]);
+                args[5]);
     }
 
     public void run(String localeDef,
                     String tournamentDescriptionFilePath,
                     String playersFilePath,
                     String gamesFilePath,
-                    String eloRatingStorageFilePath,
-                    String kValueStorageFilePath,
                     String levelPlayersFilePath,
                     String outputDir) throws IOException, TemplateException {
         Configuration configuration = createTemplatesConfiguration();
@@ -57,8 +53,6 @@ public class PlayoffSiteGenerator {
         PlayoffSetup playoffSetup = (PlayoffSetup) tournamentDescription.getTournamentSetup();
         PlayerStorage playerStorage = readPlayers(playersFilePath, tournamentDescription);
         GameStorage gameStorage = readGames(gamesFilePath, tournamentDescription);
-        EloRatingReadOnlyStorage eloRatingStorage = readEloRatings(eloRatingStorageFilePath, tournamentDescription);
-        KValueReadOnlyStorage kValueStorage = readKValues(kValueStorageFilePath);
         List<PlayoffLevelPlayers> playoffLevelPlayers = readPlayoffLevelPlayers(levelPlayersFilePath);
 
         //noinspection ResultOfMethodCallIgnored
@@ -70,7 +64,7 @@ public class PlayoffSiteGenerator {
         for (PlayoffLevelPlayers levelPlayers : playoffLevelPlayers) {
             PlayoffLevelTable playoffLevelTable = new PlayoffLevelTable(
                     levelPlayers.getLevel(), levelPlayers, playerStorage,
-                    gameStorage, eloRatingStorage, kValueStorage,
+                    gameStorage,
                     playoffSetup, tournamentDescription);
             PlayoffLevelTableView view = new PlayoffLevelTableView(
                     locale, playoffLevelTable, tournamentDescription);
@@ -160,84 +154,6 @@ public class PlayoffSiteGenerator {
         String country = strings.length > 1 ? strings[1] : "";
         String variant = strings.length > 2 ? strings[2] : "";
         return language.isEmpty() ? Locale.US : new Locale(language, country, variant);
-    }
-
-    private KValueReadOnlyStorage readKValues(String kValueStorageFilePath) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        List<KValuesJsonRecord> records;
-        try (FileInputStream fis = new FileInputStream(kValueStorageFilePath)) {
-            records = mapper.readValue(fis, new TypeReference<ArrayList<KValuesJsonRecord>>() {});
-        }
-        Map<String, KValueSet> kValues = records.stream().collect(toMap(
-                KValuesJsonRecord::getPlayerId,
-                r -> new KValueSet(
-                        new KValue(r.getkValueSetRecord().getClassic()),
-                        new KValue(r.getkValueSetRecord().getRapid()),
-                        new KValue(r.getkValueSetRecord().getBlitz()))));
-        return new KValueReadOnlyStorage(kValues);
-    }
-
-    public static final class KValuesJsonRecord {
-        @JsonProperty
-        private String playerId;
-        @JsonProperty("kvalues")
-        private KValueSetRecord kValueSetRecord;
-
-        public String getPlayerId() {
-            return playerId;
-        }
-
-        public KValueSetRecord getkValueSetRecord() {
-            return kValueSetRecord;
-        }
-    }
-
-    public static final class KValueSetRecord {
-        @JsonProperty
-        private int rapid;
-        @JsonProperty
-        private int classic;
-        @JsonProperty
-        private int blitz;
-
-        public int getBlitz() {
-            return blitz;
-        }
-
-        public int getClassic() {
-            return classic;
-        }
-
-        public int getRapid() {
-            return rapid;
-        }
-    }
-
-    private EloRatingReadOnlyStorage readEloRatings(String eloRatingStorageFilePath, TournamentDescription tournamentDescription) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        List<RatingRecord> records;
-        try (FileInputStream fis = new FileInputStream(eloRatingStorageFilePath)) {
-            records = mapper.readValue(fis, new TypeReference<ArrayList<RatingRecord>>() {});
-        }
-        Map<EloRatingKey, EloRating> ratings = records.stream().collect(toMap(
-                r -> new EloRatingKey(tournamentDescription.getStartDay().toInstant(), r.getPlayerId()),
-                r -> r.getRating() != null ? new EloRating(r.getRating()) : new EloRating(new BigDecimal(1500.00).setScale(2, BigDecimal.ROUND_HALF_UP))));
-        return new EloRatingReadOnlyStorage(ratings);
-    }
-
-    public static final class RatingRecord {
-        @JsonProperty
-        private String playerId;
-        @JsonProperty
-        private BigDecimal rating;
-
-        public String getPlayerId() {
-            return playerId;
-        }
-
-        public BigDecimal getRating() {
-            return rating;
-        }
     }
 
     private GameStorage readGames(String gamesFilePath, TournamentDescription tournamentDescription) throws IOException {
